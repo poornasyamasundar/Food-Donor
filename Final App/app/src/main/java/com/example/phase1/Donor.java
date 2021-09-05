aa46fb783eff9d98d66218ba48d2616ad7594d43
GannaYashwanth <73212098+GannaYashwanth@users.noreply.github.com>
2021-09-05 20:11:46 +0530

    Update Donor.java

--- Final App/app/src/main/java/com/example/phase1/Donor.java
+++ Final App/app/src/main/java/com/example/phase1/Donor.java
@@ -1,3 +1,17 @@
+
+/*
+
+This class wraps all the details that belong to a particualr user.
+This is a class whose instance stores the information of a donar who has registered for our services.
+This class has the details like the name, address, phone number, type , email , noOfDonations.
+This class is created so that whenever we want to use these details or manipulate these details that belong to a particular user, everything would be dealt with the objects of 
+this type.
+Donar objects will be passed to all the methods that want to use/manipulate these details.
+SO , it would be easier to deal with the object of this type
+So, this class has only getters and setters to deal with the data variables.
+
+*/
+
 package com.example.phase1;
 
 /*
