39e00a55c36b953fc236ef142c26da1dff589a19
GannaYashwanth <73212098+GannaYashwanth@users.noreply.github.com>
2021-09-05 20:12:55 +0530

    Update Receiver.java

--- Final App/app/src/main/java/com/example/phase1/Receiver.java
+++ Final App/app/src/main/java/com/example/phase1/Receiver.java
@@ -2,6 +2,7 @@ package com.example.phase1;
 
 /*
 This class defines the Receiver Object all the references of Receiver in the app will be made as an object of this class
+This class is same as the Donar class. So we simply extended the Donar class.
  */
 public class Receiver extends Donor
 {
